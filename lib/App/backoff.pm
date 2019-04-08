d60d3b57a4af4e391484a973003970b2973eabf8
perlancar (on pc-office) <perlancar@gmail.com>
2019-04-08 19:18:15 +0700

    Initial commit

---
+++ lib/App/backoff.pm
@@ -0,0 +1,17 @@
+package App::backoff;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Backoff using one of several available strategies
+
+
+=head1 SYNOPSIS
+
+See the included script L<backoff>.
+
+
+=head1 SEE ALSO
+
+L<Algorithm::Retry>
